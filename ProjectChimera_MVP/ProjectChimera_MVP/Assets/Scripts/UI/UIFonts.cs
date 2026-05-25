a542146f07b55ad0c8caa46a5f13852e887fa6bc
sugu <1037175596@qq.com>
2026-05-25 20:09:08 +0800

    fix: 修复 UIFonts.cs 重复方法和缺失花括号

--- ProjectChimera_MVP/ProjectChimera_MVP/Assets/Scripts/UI/UIFonts.cs
+++ ProjectChimera_MVP/ProjectChimera_MVP/Assets/Scripts/UI/UIFonts.cs
@@ -57,19 +57,3 @@ public static class UIFonts
         return tmp;
     }
 }
-
-    public static TextMeshProUGUI CreateLabel(GameObject parent, string name, string text, int fontSize, Color color)
-    {
-        var go = new GameObject(name, typeof(RectTransform));
-        go.layer = 5;
-        go.transform.SetParent(parent.transform, false);
-        go.AddComponent<CanvasRenderer>();
-        var tmp = go.AddComponent<TextMeshProUGUI>();
-        tmp.text = text;
-        tmp.fontSize = fontSize;
-        tmp.color = color;
-        tmp.alignment = TextAlignmentOptions.Center;
-        Apply(tmp);
-        return tmp;
-    }
-}
